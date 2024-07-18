1ccee541f4a785afc1cf6d6b7089abfefe966a5d
JeongEuiwang <98511234+JeongEuiWang@users.noreply.github.com>
2024-07-18 17:20:04 +0900

    config: pwa setting

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import react from '@vitejs/plugin-react';
 import * as path from 'path';
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
+import manifest from './manifest.json';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,6 +11,20 @@ export default defineConfig({
       jsxImportSource: '@emotion/react',
       babel: {
         plugins: ['@emotion/babel-plugin'],
+      },
+    }),
+    VitePWA({
+      manifest: {
+        ...manifest,
+        display: 'standalone',
+      },
+      includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+      // switch to "true" to enable sw on development
+      devOptions: {
+        enabled: true,
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html}', '**/*.{svg,png,jpg,gif}'],
       },
     }),
   ],

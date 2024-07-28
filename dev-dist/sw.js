612ddf56e345a3a421daf3b830e838e3fc083955
JeongEuiwang <98511234+JeongEuiWang@users.noreply.github.com>
2024-07-28 23:16:18 +0900

    style: add full screen size, background-color

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-9637eeee'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.340b8nj3bs8"
+    "revision": "0.shvgeis1638"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {

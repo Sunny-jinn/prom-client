21127913a6f94387421c95bb13b5818b113c6395
Sunny-jinn <rlawlsdn316@gmail.com>
2024-09-21 23:55:38 +0900

    feat: add follow page router

--- src/RootRouter.tsx
+++ src/RootRouter.tsx
@@ -1,33 +1,35 @@
-import { Outlet, Route, Routes } from 'react-router-dom';
-import OnBoarding from '@/pages/OnBoarding';
 import { useEffect, useState } from 'react';
+import { Outlet, Route, Routes } from 'react-router-dom';
+import { refreshAPI } from '@/feature/api/user.api';
 import useAppNavigate from '@/hooks/useAppNavigate';
+import Home from '@/pages/Home';
+import Init from '@/pages/Init';
+import OnBoarding from '@/pages/OnBoarding';
 import SignIn from '@/pages/SignIn';
 import Splash from '@/pages/Splash';
-import { refreshAPI } from '@/feature/api/user.api';
-import Init from '@/pages/Init';
 import userStore from '@/store/User';
+import FollowList from './pages/FollowList';
 import MyPage from './pages/MyPage';
 import MyPageAllPosts from './pages/MyPageAllPosts';
 import PostDetail from './pages/PostDetail';
 import SignUp from './pages/SignUp';
-import Home from '@/pages/Home';
 
 const RootRouter = () => {
   return (
     <Routes>
       <Route index element={<div>웹사이트</div>} />
-      <Route path='app/*' element={<AppRoute />}>
-        <Route path='on-board' element={<OnBoarding />} />
-        <Route path='sign-in' element={<SignIn />} />
-        <Route path='sign-up' element={<SignUp />} />
+      <Route path="app/*" element={<AppRoute />}>
+        <Route path="on-board" element={<OnBoarding />} />
+        <Route path="sign-in" element={<SignIn />} />
+        <Route path="sign-up" element={<SignUp />} />
         <Route element={<Auth />}>
-          <Route path='init' element={<Init />} />
-          <Route path='home' element={<Home/>} />
+          <Route path="init" element={<Init />} />
+          <Route path="home" element={<Home />} />
         </Route>
         <Route path="my-page/*">
           <Route index element={<MyPage />} />
           <Route path={'all-posts'} element={<MyPageAllPosts />} />
+          <Route path={'follow-list/:status'} element={<FollowList />} />
         </Route>
         <Route path="post/:post_id" element={<PostDetail />} />
       </Route>
@@ -41,13 +43,13 @@ export default RootRouter;
 const AppRoute = () => {
   const navigate = useAppNavigate();
   const [appLoading, setAppLoading] = useState(true);
-  const { setUser } = userStore(state => state);
+  const { setUser } = userStore((state) => state);
 
   const refresh = async () => {
     try {
       const result = await refreshAPI();
       setUser(result);
-      if(result.role === 'USER') {
+      if (result.role === 'USER') {
         navigate('init');
         return;
       }
@@ -65,7 +67,7 @@ const AppRoute = () => {
     //1-2. 로그인 된 유저 중 초기 세팅을 완료한 유저일 경우 status 1 => 메인 화면
     refresh();
   }, []);
-  if(appLoading) {
+  if (appLoading) {
     return <Splash />;
   }
   return <Outlet />;

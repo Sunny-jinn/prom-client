b7d71ce27b0f00dbdee691ab8f4768266958e4b6
Sunny-jinn <rlawlsdn316@gmail.com>
2024-09-30 02:02:06 +0900

    feat: fetch follow list

--- src/pages/FollowList.tsx
+++ src/pages/FollowList.tsx
@@ -1,18 +1,31 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
 import { Tab, TabIndicator, TabList, TabPanel, TabPanels, Tabs } from '@chakra-ui/react';
 import icon_sort from '@/assets/img/icon_sort.png';
 import icon_left_arrow from '@/assets/img/left_arrow.png';
-import dummy_profile from '@/assets/img/profile_background.png';
 import CustomSearchInput from '@/components/CustomSearchInput';
 import FollowUserCard from '@/components/FollowUserCard';
 import { SafeAreaLayout } from '@/components/SafeAreaLayout';
+import followerStore from '@/store/Follow';
 import './FollowList.scss';
 
 const FollowList = () => {
   const [tabIndex, setTabIndex] = useState<number>(0);
 
   const navigate = useNavigate();
+
+  const { status } = useParams();
+
+  useEffect(() => {
+    status === 'follower' ? setTabIndex(0) : setTabIndex(1);
+  }, []);
+
+  const { follower, following } = followerStore((state) => ({
+    follower: state.follower,
+    following: state.following,
+  }));
+
+  console.log(follower, following);
 
   return (
     <SafeAreaLayout flexDirection="column">
@@ -26,13 +39,22 @@ const FollowList = () => {
             <img src={icon_left_arrow} alt="left" />
           </button>
         </div>
-        <Tabs isFitted variant={'unstyled'} onChange={(index) => setTabIndex(index)}>
+        <Tabs
+          isFitted
+          variant={'unstyled'}
+          onChange={(index) => setTabIndex(index)}
+          defaultIndex={status === 'follower' ? 0 : 1}
+        >
           <TabList>
             <Tab p={'14px'}>
-              <span className={`follow-list-title ${tabIndex === 0 && 'active'}`}>33 팔로워</span>
+              <span className={`follow-list-title ${tabIndex === 0 && 'active'}`}>
+                {follower.length} 팔로워
+              </span>
             </Tab>
             <Tab>
-              <span className={`follow-list-title ${tabIndex === 1 && 'active'}`}>16 팔로잉</span>
+              <span className={`follow-list-title ${tabIndex === 1 && 'active'}`}>
+                {following.length} 팔로잉
+              </span>
             </Tab>
           </TabList>
           <TabIndicator height={'2px'} bg={'#7Bf7ff'} />
@@ -48,8 +70,12 @@ const FollowList = () => {
                 </div>
               </div>
               <div className="follow-list-users">
-                {DUMMY_USERS.map((item) => (
-                  <FollowUserCard profile={item.profile} name={item.name} key={item.id} />
+                {follower.map((item) => (
+                  <FollowUserCard
+                    profile={item.profileImageUrl}
+                    name={item.nickname}
+                    key={item.profileImageUrl}
+                  />
                 ))}
               </div>
             </TabPanel>
@@ -65,8 +91,12 @@ const FollowList = () => {
                 </div>
               </div>
               <div className="follow-list-users">
-                {DUMMY_USERS.map((item) => (
-                  <FollowUserCard profile={item.profile} name={item.name} key={item.id} />
+                {following.map((item) => (
+                  <FollowUserCard
+                    profile={item.profileImageUrl}
+                    name={item.nickname}
+                    key={item.profileImageUrl}
+                  />
                 ))}
               </div>
             </TabPanel>
@@ -76,33 +106,5 @@ const FollowList = () => {
     </SafeAreaLayout>
   );
 };
-
-const DUMMY_USERS = [
-  {
-    id: 0,
-    profile: dummy_profile,
-    name: 'OffTheMenu',
-  },
-  {
-    id: 1,
-    profile: dummy_profile,
-    name: 'RYE',
-  },
-  {
-    id: 2,
-    profile: dummy_profile,
-    name: '랜드오브피스',
-  },
-  {
-    id: 3,
-    profile: dummy_profile,
-    name: '권나무',
-  },
-  {
-    id: 4,
-    profile: dummy_profile,
-    name: 'Axl and Goldman',
-  },
-];
 
 export default FollowList;

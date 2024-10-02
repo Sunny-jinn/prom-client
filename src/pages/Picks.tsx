024f3561ebf90a0d95080fa2e5c6ca4757c9c3cc
JeongEuiwang <98511234+JeongEuiWang@users.noreply.github.com>
2024-10-03 02:08:32 +0900

    feat: getPick done

--- src/pages/Picks.tsx
+++ src/pages/Picks.tsx
@@ -3,25 +3,39 @@ import { useLocation } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import NavigatorLayout from '@/components/NavigatorLayout';
 import PickViewer from '@/components/PickViewer';
-import { getPicksAPI } from '@/feature/api/post.api';
+import { deleteCacheAPI, getPicksAPI } from '@/feature/api/post.api';
 
 const Picks = () => {
   const location = useLocation();
   const params = new URLSearchParams(location.search);
 
   const [pickIds, setPickIds] = useState<number[]>([]);
+  const [currentIndex, setCurrentIndex] = useState<number>(0)
   // query 확인 후 index id가 존재하면 해당 id를 첫번째 index로 설정
   // 없을 경우 randomly하게 조회한 아이디들로 배열 구성
   const getPicks = async () => {
     try {
       const result = await getPicksAPI({
         orderBy: 'rand',
-      });
-      console.log(result);
+        size: 5
+      })
+      if(result.length === 0){
+        return;
+      }
+      const filteredId = result.filter(el => !pickIds.includes(el.shortFormId)).map(el => el.shortFormId)
+      setPickIds(prev => prev.concat([...filteredId]))
     } catch (e) {
       console.log(e);
     }
   };
+
+  const removeCache = async() => {
+    try{
+      await deleteCacheAPI();
+    }catch (e) {
+      console.log(e);
+    }
+  }
 
   useEffect(() => {
     if(params.get('index') !== null) {
@@ -34,14 +48,24 @@ const Picks = () => {
   }, []);
 
   useEffect(() => {
+    if((pickIds.length - 1) ===  currentIndex){
+      getPicks();
+    }
+  }, [pickIds, currentIndex])
+
+  console.log(currentIndex);
+  console.log(pickIds);
+
+  useEffect(() => {
     return () => {
+      removeCache();
       //unmount 될 때 onClose
     };
   }, []);
 
   return (
     <NavigatorLayout hasScrollArea={false}>
-      <PickViewer pickIds={pickIds} />
+      <PickViewer setCurrentIndex={setCurrentIndex} pickIds={pickIds} />
     </NavigatorLayout>
   );
 };

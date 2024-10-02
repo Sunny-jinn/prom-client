775db84a51b31552aa9298e65824b8188f6c0a21
Sunny-jinn <rlawlsdn316@gmail.com>
2024-10-03 00:46:43 +0900

    feat: fix search result card

---
+++ src/components/SearchPostLists.tsx
@@ -0,0 +1,86 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { PostFeed, PostPick } from '@/feature/types/Post.type';
+
+const renderFeedShorts = (feeds: PostFeed[], shorts: PostPick[]) => {
+  const feedChunkSize = 4;
+  const maxShorts = Math.min(shorts.length, Math.floor(feeds.length / feedChunkSize));
+
+  // 필요한 만큼 feeds와 shorts를 나눔
+  const displayedFeeds = feeds.slice(0, maxShorts * feedChunkSize); // 필요한 feeds만 가져옴
+  const displayedShorts = shorts.slice(0, maxShorts); // 필요한 shorts만 가져옴
+
+  // 그룹화하여 하나의 세트로 만듦
+  const feedShortsChunks = [];
+  for (let i = 0; i < maxShorts; i++) {
+    const feedChunk = displayedFeeds.slice(i * feedChunkSize, (i + 1) * feedChunkSize);
+    const short = displayedShorts[i];
+    feedShortsChunks.push({ feeds: feedChunk, short });
+  }
+
+  return feedShortsChunks;
+};
+
+type SearchPostListsProps = {
+  feeds: PostFeed[];
+  picks: PostPick[];
+};
+
+export const SearchPostLists = ({ feeds, picks }: SearchPostListsProps) => {
+  const feedShortsChunks = renderFeedShorts(feeds, picks);
+
+  const naviagte = useNavigate();
+
+  return (
+    <div className="search-post-list-container">
+      {feedShortsChunks.map((chunk, index) => (
+        <React.Fragment key={index}>
+          {/* 짝수 인덱스일 때는 post cards가 왼쪽, shorts가 오른쪽 */}
+          {index % 2 === 0 ? (
+            <>
+              <div className="search-post-list-cards">
+                {chunk.feeds.map((feed, feedIndex) => (
+                  <div
+                    className="search-post-list-card"
+                    key={feedIndex}
+                    onClick={() => naviagte(`/app/post/${feed.feedId}`)}
+                  >
+                    <img src={feed.images[0]} alt="Feed" />
+                  </div>
+                ))}
+              </div>
+              <div className="search-post-list-shorts">
+                <div
+                  className="search-post-list-shorts-card"
+                  onClick={() => naviagte(`/app/pick/${chunk.short.shortFormId}`)}
+                >
+                  <img src={chunk.short.thumbnailUrl} alt="Short" />
+                </div>
+              </div>
+            </>
+          ) : (
+            /* 홀수 인덱스일 때는 shorts가 왼쪽, post cards가 오른쪽 */
+            <>
+              <div className="search-post-list-shorts">
+                <div className="search-post-list-shorts-card">
+                  <img src={chunk.short.thumbnailUrl} alt="Short" />
+                </div>
+              </div>
+              <div className="search-post-list-cards">
+                {chunk.feeds.map((feed, feedIndex) => (
+                  <div
+                    className="search-post-list-card"
+                    key={feedIndex}
+                    onClick={() => naviagte(`/app/post/${feed.feedId}`)}
+                  >
+                    <img src={feed.images[0]} alt="Feed" />
+                  </div>
+                ))}
+              </div>
+            </>
+          )}
+        </React.Fragment>
+      ))}
+    </div>
+  );
+};
--- src/components/SearchResultCard.tsx
+++ src/components/SearchResultCard.tsx
@@ -1,6 +1,8 @@
+import { useNavigate } from 'react-router-dom';
 import styled from '@emotion/styled';
 import icon_delete from '@/assets/img/icon_delete_recent.svg';
 import icon_search from '@/assets/img/icon_search_square.svg';
+import userStore from '@/store/User';
 
 const Wrapper = styled.div`
   display: flex;
@@ -36,11 +38,24 @@ type SearchResultCardProps = {
   isRecent?: boolean;
   text: string;
   profile?: string;
+  id?: string;
 };
 
-const SearchResultCard = ({ isRecent, text, profile }: SearchResultCardProps) => {
+const SearchResultCard = ({ isRecent, text, profile, id }: SearchResultCardProps) => {
+  const navigate = useNavigate();
+
+  const { user } = userStore();
+
+  const clickHandler = (id: string | undefined) => {
+    if (String(user?.id) === id) {
+      navigate('/app/my-page');
+    } else {
+      navigate(`/app/profile/${id}`);
+    }
+  };
+
   return (
-    <Wrapper>
+    <Wrapper onClick={() => clickHandler(id)}>
       <IconBox>
         {isRecent ? <img src={icon_search} alt="" /> : <img src={profile} alt="" />}
       </IconBox>
